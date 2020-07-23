31ff2550abc68dc863c4d9f9b2b9f27ef04fa566
Jack Moffitt <metajack@fb.com>
2020-07-23 16:20:33 -0500

    add mock ci workflow that comments on PRs

---
+++ .github/actions/post-comment/src/index.js
@@ -0,0 +1,31 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const got = require("got");
+const process = require("process");
+
+async function main() {
+  try {
+    const { owner, repo, number } = github.context.issue;
+    const comment = core.getInput("comment", {required: true});
+
+    // trigger the hyperjump
+    const body = {
+      owner: owner,
+      repo: repo,
+      type: "comment",
+      args: {
+        number: number,
+        comment: comment,
+      },
+    };
+    const server = "http://metajack.im:9898/hyperjump/jump"; //process.env.HYPERJUMP_URL;
+    await got.post(process.env.HYPERJUMP_SERVER, {
+      retry: 0,
+      json: body,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
