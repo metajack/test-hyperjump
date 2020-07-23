47b8dbb3a3c58b345b15875fb5d01d7bc46572b1
Jack Moffitt <metajack@fb.com>
2020-07-23 15:19:03 -0500

    initial commit to test comment hyperjump

---
+++ .github/actions/comment/src/index.js
@@ -0,0 +1,24 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function main() {
+  try {
+    const github_token = core.getInput("github-token", {required: true});
+    const comment = core.getInput("comment", {required: true});
+
+    const client = new github.getOctokit(github_token);
+    const context = github.context;
+    const issue = context.issue;
+
+    await client.issues.createComment({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+      body: comment,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
