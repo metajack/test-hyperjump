b345f77d123faa7c991eba86afa8e4f1b9a41190
Jack Moffitt <metajack@fb.com>
2020-09-09 15:48:35 -0500

    fix args

--- .github/actions/request-review/src/index.js
+++ .github/actions/request-review/src/index.js
@@ -8,9 +8,19 @@ const hyperjump_url = "http://github.aws.hlw3truzy4ls.com:6080/hyperjump/jump";
 async function main() {
   try {
     const { owner, repo, number } = github.context.issue;
-    const reviewers = core.getInput("reviewers", {required: true});
+    const users = core.getInput("users", {required: false});
+    const teams = core.getInput("teams", {required: false});
 
-    const reviewer_list = (reviewers || "")
+    if (!users && !teams) {
+      core.warning("users and teams fields both empty");
+      return;
+    }
+
+    const user_list = (users || "")
+          .split(",")
+          .map(s => s.trim())
+          .filter(s => s.length > 0);
+    const team_list = (teams || "")
           .split(",")
           .map(s => s.trim())
           .filter(s => s.length > 0);
@@ -22,7 +32,8 @@ async function main() {
       type: "request-review",
       args: {
         number: number,
-        reviewers: reviewer_list,
+        users: user_list,
+        teams: team_list,
       },
     };
     await got.post(hyperjump_url, {
